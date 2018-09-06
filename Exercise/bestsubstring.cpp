68c41f0241aacc8a468c1609384ac735dd934eb8
cchenlife <42125970+cchenlife@users.noreply.github.com>
2018-09-06 17:15:09 +0800

    Update bestsubstring.cpp

--- Exercise/bestsubstring.cpp
+++ Exercise/bestsubstring.cpp
@@ -1,6 +1,8 @@
 给定一个有小写字母组成的字符串S，将其分割成尽量多的子串，并保证每个字母最多只在其中一个子串中出现。
 请返回由一个或多个整数表示的分割后各个子串的长度。如：‘ababbacadefgdehijhklij’=  ‘ababbaca‘ + ’defgde‘+ ’hijhklij‘。
 
+https://blog.csdn.net/liqinzhe11/article/details/79165588
+
 #include<iostream>
 #include<vector>
 #include<string>
@@ -15,26 +17,20 @@ int main(){
 	vector<int> results;
 	for (int i = 0; i < n; i++)
 		ends[i] = str.rfind(str[i]);
-	int start = 0;
-	int result = ends[start];
-	while (result < n){
-		int i = start;
-		for (; i < result; i++){
-			if (ends[i]>result){
-				result = ends[i];
-				break;
-			}
+	int start = 0, index = 0, end = 0;
+	while (index < n)
+	{
+		end = ends[start];
+		while (index < end)
+		{
+			end = max(end, ends[index]);
+			index++;
 		}
-		if (i >= result - 1){
-			results.push_back(result);
-			if (result + 1 >= n)
-				break;
-			start = result + 1;
-			result = ends[start];
-		}
-
-
+		results.push_back(end);
+		index++;
+		start = index;
 	}
+	
 	int cutstart = 0;
 	for (int i = 0; i < results.size(); i++){
 		cout << cutstart<<" "<<results[i] << " " << str.substr(cutstart, results[i]-cutstart) << endl;
